760b1cf073723c63693b51b9cd5702a804357af1
Dave Page <dpage@pgadmin.org>
2021-03-24 18:43:22 +0000

    Make the default security key more obviously something to override in settings_local.py

--- feedagg/settings.py
+++ feedagg/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bzm)9i0slassm!+ef1&cufj^x9awufluxaj@l4k82kv3%0(3)('
+SECRET_KEY = 'OVERRIDE ME!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
