99919e45a82d5fcd1348a516a14bb89c611b81d5
Dave Page <dpage@pgadmin.org>
2023-01-30 11:59:36 +0000

    Improve admin display

--- feeds/admin.py
+++ feeds/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from django.db.models import TextField
+from django.forms import Textarea
+from django.utils.safestring import mark_safe
+
 from . import models
 
 
@@ -18,15 +22,19 @@ class PostAdmin(admin.ModelAdmin):
 class PollErrorAdmin(admin.ModelAdmin):
     list_display = ('error_time', 'in_feed', 'error_summary')
     list_filter = ('in_feed', 'error_code')
+    date_hierarchy = 'error_time'
+    readonly_fields = ('error_time', 'in_feed', 'error_summary', 'error_code',)
+    fields = ('error_time', 'in_feed', 'error_summary', 'error_code', 'error_message')
 
-    def has_add_permission(self, request):
+    def has_add_permission(self, request, obj=None):
         return False
 
-    def has_change_permission(self, request, obj=None):
+    def has_update_permission(self, request, obj=None):
         return False
 
-    def has_delete_permission(self, request, obj=None):
-        return False
+    formfield_overrides = {
+        TextField: dict(widget=Textarea(attrs=dict(readonly=True)))
+    }
 
 
 admin.site.register(models.OutFeed, OutFeedAdmin)
